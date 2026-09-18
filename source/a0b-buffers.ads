60619554b949334b3f73dd1a92be14bc13405a21
Vadim Godunko <vgodunko@gmail.com>
2026-09-18 09:04:45 +0400

    Stub of the package

---
+++ source/a0b-buffers.ads
@@ -0,0 +1,11 @@
+--
+--  Copyright (C) 2026, Vadim Godunko
+--
+--  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+--
+
+package A0B.Buffers
+  with Pure, No_Elaboration_Code_All
+is
+
+end A0B.Buffers;
